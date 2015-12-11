f61f685c657f4785aecc9b2fd5c14c0d521f902b
Roman Potashow <justgook@gmail.com>
2015-12-11 12:38:58 +0200

    Build script update

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-rm -rf public || exit 0;
-npm run build
-( cd public
- git init
- git config user.name "Travis-CI"
- git config user.email "travis@nodemeatspace.com"
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages #> /dev/null 2>&1
-)
+cd public
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@nodemeatspace.com"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages #> /dev/null 2>&1
